92658cd6a07542e6be357713274623edddaa78e3
monster-1951 <shivashankarsince19@gmail.com>
2025-03-30 08:03:00 +0530

    registration schema modified

--- src/schemas/registerSchema.ts
+++ src/schemas/registerSchema.ts
@@ -6,7 +6,6 @@ export const userNameValidation = z
   .string()
   .min(2, "Username must be atleast 2 characters")
   .max(30, "Username must be less than 20 characters")
-  .regex(/^[a-zA-Z0-9_]+$/, "Username must not contain special characters");
 
 export const passwordSchema = z
   .string()
